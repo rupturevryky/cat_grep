370f036ce5da70882dca417b9de6ac046b3fb1ea
Rupturevryky <->
2024-05-01 01:47:05 +0300

    colorize grep

--- grep.c
+++ grep.c
@@ -19,19 +19,21 @@ typedef struct {
 
 void check_opt(MyObject *obj, int argc, char *argv[]);
 
-void infinity_input();
+void infinity_input(char *pattern);
 
 int main(int argc, char *argv[]) {
     MyObject obj = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
     check_opt(&obj, argc, argv);
 
-    // Проверяем, что переданы аргументы командной строки
-    if (optind >= argc) {
+    // Проверяем, что передан патерн
+    if (optind == argc) {
         printf("Usage: grep [OPTION]... PATTERNS [FILE]...");
         return 0;
     }
-    infinity_input(&obj);
+    char *pattern = argv[optind];
+
+    if (optind + 1 == argc) infinity_input(pattern);
 
     return 0;
 }
@@ -79,9 +81,48 @@ void check_opt(MyObject *obj, int argc, char *argv[]) {
     }
 }
 
-void infinity_input() {
+char *next_found(char *input, char *pattern, int *pattern_index, char *found, int *pattern_end,
+                 int *updateble, int pattern_len) {
+    int tmp = *pattern_index;
+    found = strstr(found + 1, pattern);
+    *pattern_index = found - input;
+    if (tmp == *pattern_index) {
+        *updateble = 0;
+        return found;
+    }
+    *pattern_end = *pattern_index + pattern_len - 1;
+    return found;
+}
+
+void colorize_line(char *input, char *pattern, int pattern_len) {
+    char *found = strstr(input, pattern);
+
+    if (found != NULL) {
+        int pattern_index = found - input;
+        int updateble = 1;
+        int pattern_end = pattern_index + pattern_len - 1;
+
+        for (int i = 0; input[i] != '\0'; i++) {
+            if (i >= pattern_index && i <= pattern_end)
+                printf("\x1b[31m%c\x1b[0m", input[i]);
+            else if (updateble && i > pattern_end && found != NULL) {
+                found =
+                    next_found(input, pattern, &pattern_index, found, &pattern_end, &updateble, pattern_len);
+                i--;
+                continue;
+            } else
+                printf("%c", input[i]);
+        }
+        printf("\n");
+    }
+}
+
+void infinity_input(char *pattern) {
     char input[8192];
+    int pattern_len = strlen(pattern);
     while (fgets(input, sizeof(input), stdin) != NULL) {
-        printf("\x1b[31m%s\x1b[0m", input);
+        input[strcspn(input, "\n")] = '\0';  // Удаляем символ новой строки
+
+        colorize_line(input, pattern, pattern_len);
     }
 }
