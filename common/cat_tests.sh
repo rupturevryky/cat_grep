55600c16e3a3c3f218677d524474939c958710ba
Rupturevryky <->
2024-05-09 15:13:39 +0300

    cat tests

---
+++ common/cat_tests.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# без флагов
+cat ./txt/*.txt > test_cat1.txt
+../cat/s21_cat ./txt/*.txt > test_cat2.txt
+diff test_cat1.txt test_cat2.txt
+rm -rf test_cat1.txt test_cat2.txt
+
+# -b --number-nonblank
+cat -b ./txt/*.txt > test_cat1.txt
+../cat/s21_cat -b ./txt/*.txt > test_cat2.txt
+diff test_cat1.txt test_cat2.txt
+rm -rf test_cat1.txt test_cat2.txt
+
+cat --number-nonblank ./txt/*.txt > test_cat1.txt
+../cat/s21_cat --number-nonblank ./txt/*.txt > test_cat2.txt
+diff test_cat1.txt test_cat2.txt
+rm -rf test_cat1.txt test_cat2.txt
+
+# -e -E
+
+cat -e ./txt/*.txt > test_cat1.txt
+../cat/s21_cat -e ./txt/*.txt > test_cat2.txt
+diff test_cat1.txt test_cat2.txt
+rm -rf test_cat1.txt test_cat2.txt
+
+cat -E ./txt/*.txt > test_cat1.txt
+../cat/s21_cat -E ./txt/*.txt > test_cat2.txt
+diff test_cat1.txt test_cat2.txt
+rm -rf test_cat1.txt test_cat2.txt
+
+# -n --number
+
+cat -n ./txt/*.txt > test_cat1.txt
+../cat/s21_cat -n ./txt/*.txt > test_cat2.txt
+diff test_cat1.txt test_cat2.txt
+rm -rf test_cat1.txt test_cat2.txt
+
+cat --number ./txt/*.txt > test_cat1.txt
+../cat/s21_cat --number ./txt/*.txt > test_cat2.txt
+diff test_cat1.txt test_cat2.txt
+rm -rf test_cat1.txt test_cat2.txt
+
+# -s --squeeze-blank
+cat -s ./txt/*.txt > test_cat1.txt
+../cat/s21_cat -s ./txt/*.txt > test_cat2.txt
+diff test_cat1.txt test_cat2.txt
+rm -rf test_cat1.txt test_cat2.txt
+
+cat --squeeze-blank ./txt/*.txt > test_cat1.txt
+../cat/s21_cat --squeeze-blank ./txt/*.txt > test_cat2.txt
+diff test_cat1.txt test_cat2.txt
+rm -rf test_cat1.txt test_cat2.txt
+
+# -t -T
+cat -t ./txt/*.txt > test_cat1.txt
+../cat/s21_cat -t ./txt/*.txt > test_cat2.txt
+diff test_cat1.txt test_cat2.txt
+rm -rf test_cat1.txt test_cat2.txt
+
+cat -T ./txt/*.txt > test_cat1.txt
+../cat/s21_cat -T ./txt/*.txt > test_cat2.txt
+diff test_cat1.txt test_cat2.txt
+rm -rf test_cat1.txt test_cat2.txt
